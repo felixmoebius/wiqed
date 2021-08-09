946bc792433b2296909ad86db14e06d6313481cc
Felix Moebius <felix.moebius@posteo.de>
2021-08-09 20:30:55 +0200

    refactor

--- lib/definition.ml
+++ lib/definition.ml
@@ -7,6 +7,6 @@ let instantiate ((term, params) : t) (args : Term.t list) =
   let%bind z =
     match List.zip params args with
     | Ok z' -> Result.return z'
-    | _ -> Result.fail "todo"
+    | _ -> Result.fail "instantiating definition with incorrect number of arguments"
   in
   Result.return (List.fold z ~init:term ~f:(fun t (p, a) -> Term.subst t p a))
